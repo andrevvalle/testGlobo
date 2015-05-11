7f5083e4ac0990880ffd38661024e9486e4c8270
Andre Valle <andrevvalle@icloud.com>
2015-05-11 01:07:07 -0300

    Update

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var PostlistController = require('../api/controller/postlistController');
 
 router.get('/', function(req, res){
+	response.send(cool());
 	res.render('index', { title: "PostFeed Globo.com" });
 });
 
